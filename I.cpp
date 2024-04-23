3a563f5d05860c79a90a49b8112ffdff0719c527
gshs23078 <129289574+gshs23078@users.noreply.github.com>
2024-04-23 10:26:41 +0900

    Update I.cpp

--- I.cpp
+++ I.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
 #include <algorithm>
+#include <vector>
 using namespace std;
+#define int long long
+#define endl '\n'
+#define fori(i, a, b) for(int i=a; i<b; i++)
+#define ford(i, a, b) for(int i=a;i>=b; i--)
 int n;
-int Min=2147483647;
-int a[10]={0};
-void f(int now, int other, int depth){
-	if(now>=Min) return;
-	vector<int> oth;
-	for(int i=0; i<n; i++){
-		if(!((other>>i)&1)) oth.push_back(i);
+vector<int> v;
+int f(int now, int time){
+	int cnt=0, p=0, m=-1;
+	while(now>>p){
+		if((now>>p)%2==0 && m<0) m=p;
+		cnt+=(now>>p++)&1;
 	}
-	int num=oth.size();
-	if(num==2){
-		now+=a[oth[1]];
-		if(now<Min) Min=now;
-		return;
-	}
-	for(int i=0; i<num; i++)
-		for(int j=i+1; j<num; j++){
-			other+=((1<<oth[i])+(1<<oth[j]));
-			int k=0;
-			while(!((other>>k)&1)) k++;
-			f(now+a[oth[j]]+a[k], other-(1<<k), depth+1);
-			other-=((1<<oth[i])+(1<<oth[j]));
+	int Min=(int)1e18, k;
+	fori(i, 0, n) fori(j, i+1, n){
+		if((now>>i)&1 && (now>>j)&1){
+			if(cnt==2){
+				time+=(v[i]>v[j]?v[i]:v[j]);
+				return time;
+			}
+			int w;
+			if(m==-1) w=i;
+			else w=m<i?m:i;
+			int q=1;
+			k=f(now-(q<<i)-(q<<j)+(q<<w), time+v[j]+v[w]);
+			if(k<Min) Min=k;
 		}
-	return;
+	}
+	return Min;
 }
-int main(){
+signed main(){
 	cin >> n;
-	for(int i=0; i<n; i++)
-		cin >> a[i];
-	sort(a, a+n);
-	f(0, 0, 0);
-	cout << Min;
+	fori(i, 0, n){
+		int k;cin >> k;
+		v.push_back(k);
+	}
+	sort(v.begin(), v.end());
+	int q=1;
+	cout << f((q<<n)-1, 0);
 }
